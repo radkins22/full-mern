707e87852427b6363271aa57fd869df428f69e8d
Rachael <rma199022@gmail.com>
2025-02-16 19:40:21 -0600

    created user schema

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,1 +1,10 @@
 // user model
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true}
+});
+
+MediaSourceHandle.exports = mongoose.model('User', userSchema);
--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
 console.log("Server started");
+
+// Importing required modules
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+// Middleware
+const User = require("./models/user.model");
