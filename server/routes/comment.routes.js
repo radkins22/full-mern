5b572f1662f1f1453d17661d668d0185df81d9a5
Rachael <rma199022@gmail.com>
2025-02-26 15:55:05 -0600

    Added to comment controllers and routes

--- server/routes/comment.routes.js
+++ server/routes/comment.routes.js
@@ -1,11 +1,17 @@
 const CommentController = require("../controllers/comment.controllers");
 
-const { create, update, read, _delete } = CommentController;
+const { create, read, _delete, readOne, update, addComment, _deleteOne } =
+  CommentController;
 
 module.exports = (prefix, app) => {
   // "/api/comments"
   app.post(prefix, create);
   app.get(prefix, read);
-  app.put(prefix, update);
   app.delete(prefix, _delete);
+
+  // "/api/comments/:_id"
+  app.get(`${prefix}/:_id`, readOne);
+  app.put(`${prefix}/:_id`, update);
+  app.put(`${prefix}/:_id/comment`, addComment);
+  app.delete(`${prefix}/:_id`, _deleteOne);
 };
